9ac7bb9d646c5b05f03a05e29e5dd99a9bb71811
Arden Mendoza <ardencristophermendoza@gmail.com>
2020-05-02 16:16:18 +0800

    Refactored "Proceed to checkout" page

--- pizza-app-client/src/Components/orderPages/CheckoutPage.tsx
+++ pizza-app-client/src/Components/orderPages/CheckoutPage.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { extraTopping } from '../../api/model';
+import { extraTopping, crustType } from '../../api/model';
 import { IPizzaAppState } from '../../Store/PizzaAppStore';
-import * as OrderActions from '../../Store/actions/OrderActions';
+import { pizzaSize } from '../../api/model';
 import { IOrderPageState } from '../../Store/reducers/OrderReducers';
 import ReceiptIcon from '@material-ui/icons/Receipt';
 import { Button, Paper } from '@material-ui/core';
@@ -15,21 +15,88 @@ interface ReduxStateProps {
 interface DispatchProps {
 
 }
+const getPizzaSizePrice = (pizzaSize: pizzaSize): number => {
+    switch (pizzaSize) {
+        case 'Small':
+            return 8;
+        case 'Medium':
+            return 10;
+        case 'Large':
+            return 12;
+    }
+    return 0;
+}
+const getPizzaCrustPrice = (pizzaCrust: crustType): number => {
+    switch (pizzaCrust) {
+        case 'Thin':
+            return 2;
+        case 'Thick':
+            return 4;
+    }
+    return 0;
+}
+
+const getPizzaToppingsPrices = (toppings: extraTopping[]): number[] => {
+    const prices: number[] = [];
+    for (let i = 0; i < toppings.length; i++) {
+        if (i < 3) {
+            prices.push(0);
+        } else {
+            prices.push(0.5);
+        }
+    }
+    return prices;
+}
+
+const getPizzaToppingsWithPrices = (toppings: extraTopping[]) => {
+    const itemsWithPrice: { item: extraTopping, price: number }[] = [];
+    for (let i = 0; i < toppings.length; i++) {
+        if (i < 3) {
+            itemsWithPrice.push({ item: toppings[i], price: 0 });
+        } else {
+            itemsWithPrice.push({ item: toppings[i], price: 0.5 });
+        }
+    }
+    return itemsWithPrice;
+}
+
+const getTotalAmount = (orderPageState: IOrderPageState) => getPizzaSizePrice(orderPageState.pizzaSize).valueOf() +
+    getPizzaCrustPrice(orderPageState.crustType).valueOf() +
+    getPizzaToppingsPrices(orderPageState.extraToppings).reduce((a, b) => a + b, 0);
+
 
 const CheckoutPanelDump: React.StatelessComponent<Props & ReduxStateProps & DispatchProps> = (props) => {
     const { orderPageState } = props;
+    console.log(getPizzaToppingsWithPrices(orderPageState.extraToppings));
     return (
         <div className={'orderPage'}>
             <h1>Proceed to checkout</h1>
             <Paper variant="outlined" square style={{ width: '50%', margin: '0px auto', padding: '20px' }}>
-                <div style={{ margin: '10px' }}>
-                    <div><span className={'boldFont'}>Pizza Size:</span> {orderPageState.pizzaSize}</div>
-                    <div><span className={'boldFont'}>Pizza Crust:</span> {orderPageState.crustType}</div>
-                    <div><span className={'boldFont'}>Extra Toppings:</span> {orderPageState.extraToppings.map(t => <div>{t}</div>)}
+                <div style={{ margin: '10px', marginBottom: '30px' }}>
+                    <div className={'flexContainer'}>
+                        <div className={'flex1 boldFont'}>Pizza Size:</div>
+                        <div className={'flex1'}>{orderPageState.pizzaSize}</div>
+                        <div className={'flex1 prices'}>$ {getPizzaSizePrice(orderPageState.pizzaSize).toFixed(2)}</div>
                     </div>
-                </div>
-                <div>
-                    
+                    <div className={'flexContainer'}>
+                        <div className={'flex1 boldFont'}>Pizza Crust:</div>
+                        <div className={'flex1'}>{orderPageState.crustType}</div>
+                        <div className={'flex1 prices'}>$ {getPizzaCrustPrice(orderPageState.crustType).toFixed(2)}</div>
+                    </div>
+                    <div className={'flexContainer'}>
+                        <div className={'flex1 boldFont'}>Extra Toppings:</div>
+                        <div className={'flex2'}>
+                            {getPizzaToppingsWithPrices(orderPageState.extraToppings).map(m => {
+                                return <div className={'flex1 flexContainer'}>
+                                    <div className={'flex1'}>{m.item}</div>
+                                    <div className={'flex1 prices'}>$ {m.price.toFixed(2)}</div>
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                    <div className={'prices boldFont'}>
+                        $ {getTotalAmount(orderPageState).toFixed(2)}
+                    </div>
                 </div>
                 <Button variant="contained" color="primary" onClick={() => { }}>
                     <ReceiptIcon htmlColor={'#fff'} fontSize={'large'} />
